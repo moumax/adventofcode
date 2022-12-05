03a748e02ca63c3c25b2dce96f2bb4157051002d
Marc LANTOL <marc.lantol@gmail.com>
2022-12-05 18:44:28 +0100

    day2

---
+++ day2.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+const f = fs.readFileSync(path.join(__dirname, "./input/2.t.txt")).toString();
+
+function calculation() {
+  
+}
+
+calculation();
