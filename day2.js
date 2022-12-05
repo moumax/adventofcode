cd6d89b4ce301aad73e7e40c5ae31ba1f7dc4cf1
Marc LANTOL <marc.lantol@gmail.com>
2022-12-05 18:58:05 +0100

    update

--- day2.js
+++ day2.js
@@ -4,7 +4,21 @@ const path = require("path");
 const f = fs.readFileSync(path.join(__dirname, "./input/2.t.txt")).toString();
 
 function calculation() {
-  
+ const opponentPlay = {
+    a: "Rock",
+    b: "Paper",
+    c: "Scissors",
+ }
+ const myPlay = {
+    y: "Rock",
+    x: "Paper",
+    z: "Scissors"
+ }
+ const counter = 0
+
+ const arr = []
+const toto = f.split("\n")
+console.log(toto)
 }
 
 calculation();
